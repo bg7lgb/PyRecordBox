238eff62e3afd723fb4a95868a89e25afc925e7a
wyf <bg7lgb@gmail.com>
2017-04-12 13:22:28 +0800

    change server from gevent to wsgiref

--- PyRecordBox.py
+++ PyRecordBox.py
@@ -1,47 +1,70 @@
 #-*- coding:utf-8 -*-
 
-import sys, ctypes
+""" PyRecordBox.py """
 
-@ctypes.CFUNCTYPE(None, ctypes.c_ulong, ctypes.c_int, ctypes.c_ulong, ctypes.c_ulong, ctypes.c_ulong, ctypes.c_ulong) 
-def uboxCallback(uboxHnd, eventID, param1, param2, param3, param4):
-    print uboxHnd, eventID
+import sys
+from ctypes import *
+import ConfigParser, logging
+from logging.handlers import RotatingFileHandler
+
+from wsgiref.simple_server import make_server
+from ws4py.server.wsgirefserver import WSGIServer, WebSocketWSGIRequestHandler
+from ws4py.server.wsgiutils import WebSocketWSGIApplication
+
+from PhoneWebSocket import PhoneWebSocket
+    
+host = ''
+port = 0
+logger = logging.getLogger('PyRecordBox')
+
+def configLogger():
+    handler = RotatingFileHandler('PyRecordBox.log', maxBytes=10000000, backupCount = 5) 
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger('PyRecordBox')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
+   
+def readConfig():
+    global host, port, logger
+    config = ConfigParser.ConfigParser()
+    config.read('PyRecordBox.ini')
+    host = config.get('websocket', 'host')
+    port = int(config.get('websocket', 'port'))
+    level = config.get('log', 'level')
+
+    if level == 'INFO':
+        logger.setLevel(logging.INFO)
+    elif level == 'WARNING':
+        logger.setLevel(logging.WARNING)
+    elif level == 'DEBUG':
+        logger.setLevel(logging.DEBUG)
+    elif level == 'ERROR':
+        logger.setLevel(logging.ERROR)
+    elif level == 'CRITICAL':
+        logger.setLevel(logging.CRITICAL)
+    else:
+        logger.setLevel(logging.NOSET)
 
 class PyRecordBox(object):
-    def __init__(self):
-        self.dll = None
-
-    def readConfig(self):
-        pass
-    
-    def uboxOpen(self):
-        if self.dll:
-            self.dll.ubox_open.restype = ctypes.c_int
-            rt =  self.dll.ubox_open(uboxCallback, ctypes.c_int(0))
-            if rt:
-                print 'open device fail', rt
-            else:
-                print 'open device success'
-
-    def uboxClose(self):
-        if self.dll:
-            self.dll.ubox_close()
-        
-        
     def run(self):
-        self.dll = ctypes.WinDLL('phonic_ubox.dll')
-        if not self.dll:
-            print 'load phonic_ubox.dll failed.'
-            sys.exit(1)
-        self.uboxOpen()
+        global host, port 
+        logger.info('PyRecordBox is running...')
+        
+        self.server = make_server(host, port, server_class=WSGIServer,
+            handler_class=WebSocketWSGIRequestHandler,
+            app=WebSocketWSGIApplication(handler_cls=PhoneWebSocket))
+        self.server.initialize_websockets_manager()
+        self.server.serve_forever()
 
-        while True:
-            cmd = raw_input('>')
-            if cmd == 'quit':
-                self.uboxClose()
-                sys.exit(0)
-            else:
-                continue
+    def openDevice(self):
+        pass
 
-if __name__ == "__main__":
-    box = PyRecordBox()
-    box.run()
+if __name__ == '__main__':
+    configLogger()
+    readConfig()
+    ubox = PyRecordBox()
+    ubox.run()
+
